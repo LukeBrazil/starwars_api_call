22e1ed38512b53bc5dd58d1592d841b18a3599a4
Luke Brazil <Luke.brazil13@icloud.com>
2021-01-14 16:05:35 -0600

    itteration 2

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const peopleButton = document.getElementById("peopleButton");
+const characterButton = document.getElementById("characterButton");
+const planetButton = document.getElementById("planetButton");
+const starshipButton = document.getElementById("starshipButton");
 
-
-peopleButton.addEventListener("click", function (event) {
+characterButton.addEventListener("click", function (event) {
   event.preventDefault();
   const url = "https://swapi.dev/api/people";
   fetch(url)
@@ -9,61 +10,60 @@ peopleButton.addEventListener("click", function (event) {
       return response.json();
     })
     .then(function (data) {
-      // console.log("star wars data", data.results)
-      var starWarsPeople = data.results.map((person) => {
+      var characters = data.results.map((character) => {
         return `<div>
-            <ol>
-            <li>Name: ${person.name}</li>
-            <li>Birth Year: ${person.birth_year}</li>
-            <li>Height: ${person.height}</li>
-            <li>Gender: ${person.gender}</li>
-            </ol>
+                <ul>
+                    <li>Name: ${character.name}</li>
+                    <li>Hair Color: ${character.hair_color}</li>
+                    <li>Birth Year: ${character.birth_year}</li>
+                </ul>
             </div>`;
       });
-      document.getElementById("data").innerHTML = starWarsPeople;
+      document.getElementById('output').innerHTML = characters;
     });
 });
+
 planetButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  const url = "https://swapi.dev/api/planets";
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      // console.log("star wars data", data.results)
-      var planets = data.results.map((planet) => {
-        return `<div>
-            <ul>
-            <li>Name: ${planet.name}</li>
-            <li>Diamater: ${planet.diameter}</li>
-            <li>Climate: ${planet.climate}</li>
-            <li>Terrain: ${planet.terrain}</li>
-            </ul>
-            </div>`;
+    event.preventDefault();
+    const url = "https://swapi.dev/api/planets";
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var planets = data.results.map((planet) => {
+          return `<div>
+                  <ul>
+                      <li>Name: ${planet.name}</li>
+                      <li>Diameter: ${planet.diameter}</li>
+                      <li>Climate: ${planet.climate}</li>
+                      <li>Population: ${planet.population}</li>
+                  </ul>
+              </div>`;
+        });
+        document.getElementById('output').innerHTML = planets;
       });
-      document.getElementById("data").innerHTML = planets;
-    });
-});
+  });
+  
 starshipButton.addEventListener("click", function (event) {
-  event.preventDefault();
-  const url = "https://swapi.dev/api/starships";
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      // console.log("star wars data", data.results)
-      var starships = data.results.map((ship) => {
-        return `<div>
-            <ol>
-            <li>Name: ${ship.name}</li>
-            <li>Birth Year: ${ship.model}</li>
-            <li>Height: ${ship.manufacturer}</li>
-            <li>Gender: ${ship.passengers}</li>
-            </ol>
-            </div>`;
+    event.preventDefault();
+    const url = "https://swapi.dev/api/starships";
+    fetch(url)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var starship = data.results.map((ship) => {
+          return `<div>
+                  <ul>
+                      <li>Name: ${ship.name}</li>
+                      <li>Manufacturer: ${ship.manufacturer}</li>
+                      <li>Passengers: ${ship.passengers}</li>
+                      <li>Hyper Drive Rating: ${ship.hyperdrive_rating}</li>
+                  </ul>
+              </div>`;
+        });
+        document.getElementById('output').innerHTML = starship;
       });
-      document.getElementById("data").innerHTML = starships;
-    });
-});
+  });
+  
